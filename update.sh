0e19ff6a4584de3df19d0ca73ecfe3de18b7a7a6
Roar <roar@aiwell.no>
2022-08-16 11:53:38 +0200

    -f

--- update.sh
+++ update.sh
@@ -4,5 +4,5 @@ docker-compose down --volumes
 rm docker-compose.yml
 wget https://raw.githubusercontent.com/aiwell-ac5000/ac5000/main/docker-compose.yml
 docker-compose -f docker-compose.yml up -d
-docker image rm roarge/fw-ac5000
-docker image rm roarge/node-red-ac5000
+docker image rm roarge/fw-ac5000 -f
+docker image rm roarge/node-red-ac5000 -f
