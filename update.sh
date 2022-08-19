02512d019e6ad438969e525a970743810e0ccf55
Roar <roar@aiwell.no>
2022-08-19 15:03:41 +0200

    new

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 cp /var/lib/docker/volumes/root_node-red-data/_data/flows.json backup_flows.json
 docker-compose down --volumes
 rm docker-compose.yml
-wget https://raw.githubusercontent.com/aiwell-ac5000/ac5000/main/docker-compose.yml
-docker-compose -f docker-compose.yml up -d
 docker image rm roarge/fw-ac5000 -f
 docker image rm roarge/node-red-ac5000 -f
+wget https://raw.githubusercontent.com/aiwell-ac5000/ac5000/main/docker-compose.yml
+docker-compose -f docker-compose.yml up -d
