c13997bbbb19603108b50e583d75c9eb452572d0
Roar <roar@aiwell.no>
2022-09-02 15:22:56 +0200

    x

--- fixsetup.sh
+++ fixsetup.sh
@@ -5,6 +5,7 @@ apt-get update --allow-releaseinfo-change -y
 apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
 apt-get install --no-install-recommends chromium-browser -y
 apt install build-essential -y
+curl https://sh.rustup.rs -sSf | sh
 #curl -sSL https://get.docker.com | sh
 apt-get install libffi-dev libssl-dev -y
 apt install python3-dev -y
