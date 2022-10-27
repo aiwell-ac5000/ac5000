6d971355671ff1bc14c65ac5298364cd1ddf0c8d
Roar <roar@aiwell.no>
2022-10-27 14:41:10 +0200

    .

--- restore.sh
+++ restore.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+apt-get update --allow-releaseinfo-change -y
+softmgr update all
 printf "p\nd\n3\nn\np\n3\n2785280\n\nN\nw\n" | fdisk /dev/mmcblk0
 restore_settings -r
 bash ex_card_configure.sh
