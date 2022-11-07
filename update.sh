049503998872ecef20f7f3a7e377e08eced589c7
Roar <roar@aiwell.no>
2022-11-07 07:36:19 +0100

    new update

--- update.sh
+++ update.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
 cp /var/lib/docker/volumes/root_node-red-data/_data/flows.json backup_flows.json
+
+softmgr update all
+
 docker-compose down --volumes
-docker image rm roarge/fw-ac5000 -f
-docker image rm roarge/node-red-ac5000 -f
+#docker image rm roarge/fw-ac5000 -f
+#docker image rm roarge/node-red-ac5000 -f
+docker-compose pull
 
 yes | docker system prune
 
@@ -13,26 +16,29 @@ rm /var/log/*.[1-9]
 apt-get update --allow-releaseinfo-change -y
 softmgr update all
 
-apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
+apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox xserver-xorg-legacy -y
 apt-get install --no-install-recommends chromium-browser -y
-apt-get purge docker docker-engine docker.io containerd runc -y
+#apt-get purge docker docker-engine docker.io containerd runc -y
 apt autoremove -y
-apt install build-essential -y
-curl https://sh.rustup.rs -sSf | sh -s -- -y
+#apt install build-essential -y
+#curl https://sh.rustup.rs -sSf | sh -s -- -y
 
-curl -sSL https://get.docker.com | sh
-apt-get install libffi-dev libssl-dev -y
-apt install python3-dev -y
-apt-get install -y python3 python3-pip
-pip3 install smbus
+#curl -sSL https://get.docker.com | sh
+#apt-get install libffi-dev libssl-dev -y
+#apt install python3-dev -y
+#apt-get install -y python3 python3-pip
+#pip3 install smbus
 
-source "$HOME/.cargo/env"
-pip3 install docker-compose
+#source "$HOME/.cargo/env"
+#pip3 install docker-compose
 
-apt install dnsmasq -y
+#apt install dnsmasq -y
 
-rustup self uninstall -y
-apt autoremove -y
+#rustup self uninstall -y
+#apt autoremove -y
+
+echo "allowed_users=console" > /etc/X11/Xwrapper.config
+echo "needs_root_rights=yes" >> /etc/X11/Xwrapper.config
 
 user=user
 upwd=AiwellAC5000
