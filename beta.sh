95a78004dff077491b132969e4ca31274b51ecfa
Roar <roar@aiwell.no>
2023-01-23 14:53:58 +0100

    -

--- beta.sh
+++ beta.sh
@@ -2,7 +2,7 @@
 # curl -sSL ac5000beta.aiwell.no | sh
 docker-compose down
 docker image rm roarge/fw-ac5000 -f
-rm docker-compose-beta.yml
+rm docker-compose.yml
 #wget https://raw.githubusercontent.com/aiwell-ac5000/ac5000/main/docker-compose-beta.yml
 wget https://raw.githubusercontent.com/aiwell-ac5000/ac5000/main/docker-compose.yml
 #mv docker-compose-beta.yml docker-compose.yml
