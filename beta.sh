03dcaa45a721b747a92f4c1d8ba6045b2c29a3d5
Roar <roar@aiwell.no>
2023-01-23 14:52:05 +0100

    _

--- beta.sh
+++ beta.sh
@@ -2,8 +2,9 @@
 # curl -sSL ac5000beta.aiwell.no | sh
 docker-compose down
 docker image rm roarge/fw-ac5000 -f
-rm docker-compose.yml
-wget https://raw.githubusercontent.com/aiwell-ac5000/ac5000/main/docker-compose-beta.yml
-mv docker-compose-beta.yml docker-compose.yml
+rm docker-compose-beta.yml
+#wget https://raw.githubusercontent.com/aiwell-ac5000/ac5000/main/docker-compose-beta.yml
+wget https://raw.githubusercontent.com/aiwell-ac5000/ac5000/main/docker-compose.yml
+#mv docker-compose-beta.yml docker-compose.yml
 yes | docker system prune
 docker-compose -f docker-compose.yml up -d
