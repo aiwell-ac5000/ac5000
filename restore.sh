2fd14bbc1b7de23407efba9b9f1a13c0229e2217
Roar <roar@aiwell.no>
2023-03-03 10:47:05 +0100

    -

--- restore.sh
+++ restore.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # curl -sSL ac5000restore.aiwell.no | sh
-apt-get update --allow-releaseinfo-change -y
-softmgr update all
+#apt-get update --allow-releaseinfo-change -y
+#softmgr update all
 printf "p\nd\n3\nn\np\n3\n2785280\n\nN\nw\n" | fdisk /dev/mmcblk0
-restore_settings -r
-bash ex_card_configure.sh
-sleep 5
+#restore_settings -r
+#bash ex_card_configure.sh
+#sleep 5
 reboot
