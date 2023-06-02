ac92d9a05343c08967ca57ee82bac1099c0925a0
aiwell-ac5000 <111335504+aiwell-ac5000@users.noreply.github.com>
2023-06-02 10:09:49 +0200

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # curl -sSL ac5000restore.aiwell.no | sh
 #apt-get update --allow-releaseinfo-change -y
-softmgr update all
-wget https://raw.githubusercontent.com/aiwell-ac5000/ac5000/main/production_portal.sh
-mv production_portal.sh /home/core/library/system_plugin/production_portal.sh 
-restore_settings -r
-printf "p\nd\n3\nn\np\n3\n2785280\n\nN\nw\n" | fdisk /dev/mmcblk0
+#softmgr update all
+#wget https://raw.githubusercontent.com/aiwell-ac5000/ac5000/main/production_portal.sh
+#mv production_portal.sh /home/core/library/system_plugin/production_portal.sh 
+restore_settings -m
+printf "p\nd\n3\nn\np\n3\n4628480\n\nN\nw\n" | fdisk /dev/mmcblk0
 #touch /root/restore
 #touch /etc/network/if-up.d/restore
 #echo "#!/bin/sh" > /etc/network/if-up.d/restore
