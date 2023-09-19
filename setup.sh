d6453ecad18b5c6b3b6312ad85947665ed3aabf8
aiwell-ac5000 <111335504+aiwell-ac5000@users.noreply.github.com>
2023-09-19 13:50:15 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -226,6 +226,7 @@ TOKEN_PART2="PywjxkDow7B52rQ0kg"
 echo "$TOKEN_PART1$TOKEN_PART2" | docker login ghcr.io -u aiwell-ac5000 --password-stdin
 curl -sSL --header "Authorization: token $TOKEN_PART1$TOKEN_PART2" -H "Accept: application/vnd.github.v3.raw" https://raw.githubusercontent.com/aiwell-ac5000/ac5000-nodes/main/subflows/digital-input/di_service.sh | sh
 #curl -sSL --header "Authorization: token $TOKEN_PART1$TOKEN_PART2" -H "Accept: application/vnd.github.v3.raw" https://raw.githubusercontent.com/aiwell-ac5000/ac5000-nodes/main/subflows/ac5000ENV/ac5000ENV.sh | sh
+curl -sSL --header "Authorization: token $TOKEN_PART1$TOKEN_PART2" -H "Accept: application/vnd.github.v3.raw" https://raw.githubusercontent.com/aiwell-ac5000/ac5000-nodes/main/subflows/setTime/setTime.sh | sh
 getenv > /root/pipes/env
 
 #wget https://raw.githubusercontent.com/aiwell-ac5000/ac5000/main/AO.py
