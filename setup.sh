d5b59ec234d6b3a59b6f19b273bd06c396a15cf3
roar <roar@aiwell.no>
2023-11-30 12:30:21 +0100

    modified text

--- setup.sh
+++ setup.sh
@@ -335,7 +335,7 @@ apt autoremove -y
 
 green='\033[0;32m'
 clear='\033[0m'
-printf "\n${green}Setup executed successfully. DO NOT PANIC. AC5000 IS SUPPOSED TO REBOOT. THIS IS NORMAL.${clear}!"
-printf "\n${green}Progammering ble korrekt utført. IKKE FÅ PANIKK. DET ER MENINGEN AT AC0500 SKAL STARTE PÅ NYTT AV SEG SELV ETTER PROGRAMMERING. DETTE ER HELT NORMALT${clear}!"
+printf "\n${green}Setup executed successfully. AC5000 IS SUPPOSED TO REBOOT. THIS IS NORMAL.${clear}!"
+printf "\n${green}Progammering ble korrekt utført. DET ER MENINGEN AT AC0500 SKAL STARTE PÅ NYTT AV SEG SELV ETTER PROGRAMMERING. DETTE ER HELT NORMALT${clear}!"
 
 reboot
