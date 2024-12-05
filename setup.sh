c1a571e80df47c980a10aa456d5f9f1a412cd819
roar <roar@aiwell.no>
2024-12-05 10:22:22 +0100

    Increase timeout for techbase updates and force firmware update in setup script

--- setup.sh
+++ setup.sh
@@ -118,6 +118,8 @@ else
     run_techbase_update "timeout 30 softmgr update core -b x500_5.10-beta"
   else
     # If not successful, use standard update
+    run_techbase_update "timeout 120 softmgr update core -f yes"
+    run_techbase_update "timeout 120 softmgr update firmware -f yes"
     run_techbase_update "timeout 30 softmgr update all"
   fi
 fi
