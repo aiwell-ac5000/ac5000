0c17385a7d50422ea5c65fd39bd27730f9bcb4fb
roar <roar@aiwell.no>
2025-02-17 11:15:22 +0100

    Add error handling for missing I2C device and create symlink to /iio_device0

--- setup_gpio.sh
+++ setup_gpio.sh
@@ -11,3 +11,12 @@ echo out > /sys/class/gpio/gpio24/direction
 
 echo 25 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio25/direction
+
+#if /sys/bus/i2c/devices/0-006c/iio:device0 does not exist, then print error message
+if [ ! -d /sys/bus/i2c/devices/0-006c/iio:device0 ]; then
+    echo "Error: i2c device not found"
+    ## create /root/busfolder
+    mkdir /root/busfolder
+    # create a symbolic link to /iio_device0
+    ln -s "/root/busfolder" /iio_device0
+fi
