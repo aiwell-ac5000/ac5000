c6f32c8ec417a235688804744802f02506275d32
roar <roar@aiwell.no>
2025-10-16 09:39:52 +0200

    Add runsetup.sh download and update .bashrc in setup script

--- setup.sh
+++ setup.sh
@@ -41,6 +41,8 @@ else
   else
     if [ "$(uname -r)" = "6.6.72-v8+" ]; then
     echo "Running on 6.6.72-v8+ kernel"
+    wget https://raw.githubusercontent.com/aiwell-ac5000/ac5000/main/runsetup.sh
+    cp runsetup.sh ~/.bashrc
     # 1056768
     printf "p\nd\n2\nn\np\n2\n1056768\n\nN\nw\n" | fdisk /dev/mmcblk0
     elif [ "$(uname -m)" = "aarch64" ]; then
@@ -54,9 +56,6 @@ else
     clear='\033[0m'
     printf "\n${green}Forsøker å utvide lagringsplassen. Systemet vil starte på nytt av seg selv${clear}!"
     printf "\n${green}Kjører setup på nytt etter omstart${clear}!"
-
-    wget https://raw.githubusercontent.com/aiwell-ac5000/ac5000/main/runsetup.sh
-    cp runsetup.sh ~/.bashrc
 
     reboot
     #exit
