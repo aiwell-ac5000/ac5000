fc9772190f2ea3eed179130e8b6e4d96c65fe917
roar <roar@aiwell.no>
2026-02-05 11:03:00 +0100

    libpython3.11-dev put back

--- update.sh
+++ update.sh
@@ -622,7 +622,7 @@ curl -sSL https://raw.githubusercontent.com/aiwell-ac5000/ac5000/main/restore_ap
 rm update
 #Remove dev tools
 apt purge docker-ce-rootless-extras mkvtoolnix fonts-urw-base35 -y
-apt purge gcc-12 g++-12 cpp-12 gdb libc6-dbg build-essential -y
+apt purge gcc-12 g++-12 cpp-12 gdb libc6-dbg libpython3.11-dev build-essential -y
 apt purge libboost1.74-dev:armhf libssl-dev libprotobuf-dev -y
 apt purge docker-buildx-plugin git firmware-realtek man-db -y
 #apt purge linux-image-6.6.51+rpt-rpi-v8 linux-image-6.6.51+rpt-rpi-2712 linux-headers-6.6.51+rpt-common-rpi -y
