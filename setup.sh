8ff75daad8dbafc60cf0464ae097db3970d5998b
amrit-aiwell <167068579+amrit-aiwell@users.noreply.github.com>
2026-02-13 11:01:06 +0100

    Update firmware update commands with new timeout and flags
    Increased timeout from 240 to 300 seconds and added '-f yes' flag for firmware update commands for new devices in stock with older linux 5.4 kernel to get HDMI to work.

--- setup.sh
+++ setup.sh
@@ -228,10 +228,10 @@ if [ "$(uname -r)" = "6.6.72-v8+" ]; then
     fi
 else
     # Run the firmware update command with a timeout
-    run_techbase_update "timeout 240 softmgr update firmware"
-    run_techbase_update "timeout 240 softmgr update core"
-    run_techbase_update "timeout 240 softmgr update lib"
-    run_techbase_update "timeout 240 softmgr update all"
+    run_techbase_update "timeout 300 softmgr update firmware -f yes"
+    run_techbase_update "timeout 300 softmgr update core -f yes"
+    run_techbase_update "timeout 300 softmgr update lib -f yes"
+    run_techbase_update "timeout 300 softmgr update all -f yes"
 fi
 
 if [ ! -f restored ]; then
